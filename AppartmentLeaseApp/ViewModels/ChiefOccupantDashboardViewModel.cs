63726a35ddd500b6417bae7fd0a124b07d785deb
AnuradhaDev96 <anusampath9470@gmail.com>
2023-01-28 20:25:25 +0530

    Role based login - create UI for chief occupant

---
+++ AppartmentLeaseApp/ViewModels/ChiefOccupantDashboardViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppartmentLeaseApp.ViewModels
+{
+    public class ChiefOccupantDashboardViewModel : Screen
+    {
+        private SimpleContainer _simpleContainer;
+
+        public ChiefOccupantDashboardViewModel(SimpleContainer simpleContainer)
+        {
+            _simpleContainer = simpleContainer;
+        }
+    }
+}
